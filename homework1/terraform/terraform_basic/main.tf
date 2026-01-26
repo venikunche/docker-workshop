35d7d2e4fc656f0bdf4067964d4856d2a8c5a64a
Veni <krisveni@gmail.com>
2026-01-26 17:29:56 +0000

    terraform portion complete

---
+++ homework1/terraform/terraform_basic/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+# Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
+#  credentials = 
+  project = "neon-metric-485516-d4"
+  region  = "us-central1"
+}
+
+
+
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "storage-bucket-hw1"
+  location      = "US"
+
+  # Optional, but recommended settings:
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
+}
+
+
+resource "google_bigquery_dataset" "dataset" {
+  dataset_id = "hw1_dataset"
+  project    = "neon-metric-485516-d4"
+  location   = "US"
+}
